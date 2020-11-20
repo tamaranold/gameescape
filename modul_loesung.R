745fa0f9e1454eb879c96a102febbf914857202e
tamaranold <nold_tamara@yahoo.de>
2020-11-20 15:44:56 +0100

    created function: compaire result with expected result, give feedback

--- modul_loesung.R
+++ modul_loesung.R
@@ -2,11 +2,43 @@
 # Modul zur Lösungseingabe
 
 # UI
-loesungUI <- function(id, data, nr) {
+loesungUI <- function(id) {
   ns <- NS(id)
-  tagList()
+  tagList(
+             numericInput(ns("los"),
+                          label = "Lösung", 
+                          value = 100,
+                          min = 100,
+                          max = 999),
+             actionButton(ns("erg"),
+                          label = "Lösung prüfen")
+    )
 }
 
 
 # Server 
-
+loesung <- function(id, data, nr) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+     observeEvent(input$erg, {
+       if(input$los == data[nr, "los"]){
+          sendSweetAlert(
+          session = session,
+          title = "Richtig!",
+          text = sample(richtig, 1),
+          type = "success"
+        )
+       }})
+      
+      observeEvent(input$erg, {
+        if(input$los != data[nr, "los"]){
+          sendSweetAlert(
+            session = session,
+            title = "Falsch!",
+            text = sample(falsch, 1),
+            type = "error"
+          )
+        }})
+    })
+}
